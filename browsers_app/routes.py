24d5f9558febb7f9bca052bbff94964a306c098c
shulepov1 <ilia.s12@yandex.ru>
2024-02-27 21:57:53 +0300

    500.html

--- browsers_app/routes.py
+++ browsers_app/routes.py
@@ -31,6 +31,10 @@ def browser(browser):
 def page_not_found(e):
     return render_template("404.html"), 404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
 @app.route("/browser/create", methods=["GET"])
 def get_form():
     return render_template("browser_form.html")
