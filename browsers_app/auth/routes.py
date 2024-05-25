62af4bc3714aa1809be9de0772fd63e95f98db38
shulepov1 <ilia.s12@yandex.ru>
2024-05-25 16:13:15 +0300

    docstrings for auth routes

--- browsers_app/auth/routes.py
+++ browsers_app/auth/routes.py
@@ -10,13 +10,18 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 @auth.context_processor
 def base():
-    # send form to base.html -> navbar.html templates
+    """
+    передавать form в base.html -> navbar.html темплейты
+    """
     form = SearchForm()
     return dict(form=form)
 
 
 @login_required
 def send_mail(to, subject, template, **kwargs):
+    """
+    метод для отправки писем
+    """
     msg = Message(subject, sender='fwson12@gmail.com', recipients=[to])
     # msg.html = render_template("confirm.html", **kwargs)
     msg.body = render_template("confirm.txt", **kwargs)
@@ -25,6 +30,10 @@ def send_mail(to, subject, template, **kwargs):
 
 @login_required
 def send_confirm(user, token):
+    """
+    метод для отправки подтверждения
+    с последующим редиректом
+    """
     send_mail(user.email, "Confirm your account",
               'confirm', user=user, token=token)
     redirect(url_for('main.dashboard'))
@@ -32,6 +41,11 @@ def send_confirm(user, token):
 
 @auth.route("/unconfirmed")
 def unconfirmed():
+    """
+    view возвращающийся при неподтвержденной почте
+    сообщение что почта не подтверждена
+    и форма /send_email_confirm
+    """
     if current_user.is_anonymous or current_user.confirmed:
         return redirect(url_for('main.index'))
     return render_template('auth/unconfirmed.html')
@@ -40,6 +54,10 @@ def unconfirmed():
 @login_required
 @auth.route("/send_email_confirm", methods=['POST'])
 def send_confirm_url():
+    """
+    action для формы,
+    отправка нового письма на почту
+    """
     if request.method == 'POST':
         try:
             token = current_user.generate_confirmation_token()
@@ -52,6 +70,11 @@ def send_confirm_url():
 
 @auth.before_app_request
 def before_request():
+    """
+    выполняется перед каждым запросом:
+    ping()
+    и если не подтвержден, редирект на unconfirmed
+    """
     if current_user.is_authenticated:
         current_user.ping()
         if not current_user.confirmed and request.blueprint != 'auth' and request.endpoint != 'static':
@@ -60,6 +83,10 @@ def before_request():
 
 @auth.route("/register", methods=['GET', 'POST'])
 def add_user():
+    """
+    страница регистрации, 
+    если post request, то попытка регистрации пользователя + письмо на почту
+    """
     if current_user.is_authenticated:
         return redirect("/dashboard")
     form = UserForm()
@@ -71,9 +98,9 @@ def add_user():
             db.session.add(user)
             db.session.commit()
             flash('User created successfully')
-            flash('Вам на почту выслано письмо с подтверждением.')
             token = user.generate_confirmation_token()
             send_confirm(user, token)
+            flash('Вам на почту выслано письмо с подтверждением.')
             return redirect(url_for('auth.login'))
     else:
         if form.username.data:
@@ -86,10 +113,12 @@ def add_user():
 @auth.route("/confirm/<token>")
 @login_required
 def confirm(token):
+    """
+    ссылка на страницу, которую пользователь получит в письме
+    подтверждение и редирект
+    """
     if not (current_user.is_authenticated):
         return "<h2>you have to be logged in to confirm your account</h2>"
-    print(token)
-    print(current_user.is_authenticated)
     if current_user.confirmed:
         return redirect(url_for('main.dashboard'))
     if current_user.confirm(token):
@@ -102,20 +131,33 @@ def confirm(token):
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
+    """
+    Страница для входа в аккаунт, редирект если уже войден
+    если сабмит формы и поля валидны, проверка
+    - что пользователь существует
+    - хэш пароля из формы совпадает с хэшом пароля
+    """
     if current_user.is_authenticated:
         return redirect("/dashboard")
     form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+
+            if not user:
+                flash("That user does not exist! Try again")
+                return render_template("login.html", form=form)
+
             if check_password_hash(user.password_hash, form.password.data):
                 login_user(user)
                 flash("Succesfull login")
                 return redirect(url_for('main.dashboard'))
-            else:
-                flash("Wrong Password! Try again.")
-        else:
-            flash("That user does not exist! Try again")
+
+            flash("Wrong Password! Try again.")
+            return render_template("login.html", form=form)
+
+        flash("fields are not valid")
 
     return render_template("login.html", form=form)
 
@@ -123,22 +165,10 @@ def login():
 @auth.route("/logout", methods=['GET', 'POST'])
 @login_required
 def logout():
+    """
+    выход из аккаунта
+    редирект на логин
+    """
     logout_user()
     flash("You have been logged out.")
     return redirect(url_for('auth.login'))
-
-
-@auth.route("/secret")
-@login_required
-def secret():
-    print("test login")
-    return "Only for auth"
-
-
-@auth.route("/testConfirm")
-@login_required
-def testConfirm():
-    user = User.query.filter_by().first()
-    tmp = user.generate_confirmation_token()
-    user.confirm(tmp)
-    return "Only for auth"
